2546f6d6f6e513faad5ea80b725d4ca764dd7bd6
Mike Karolow <mike@Mikes-Mac.local>
2013-09-11 15:41:14 -0400

    Cleanup tools install

--- site-cookbooks/yw_workstation/recipes/yw_tools.rb
+++ site-cookbooks/yw_workstation/recipes/yw_tools.rb
@@ -2,14 +2,18 @@ install_dir = "#{node['sprout']['home']}/#{node['workspace_directory']}/ywtools"
 
 git install_dir do
   repository "git@github-yesware:Yesware/tools.git"
-  destination "#{Chef::Config[:file_cache_path]}/homebrew"
+  user node['current_user']
   action :sync
   notifies :run, "execute[install-tools]"
 end
 
+directory "#{node['sprout']['home']}/bin" do
+  user node['current_user']
+end
+
 execute "install-tools" do
-  command "install-tools"
-  cwd "#{node['sprout']['home']}/#{node['workspace_directory']}/ywtools"
+  command "./install-tools"
+  cwd install_dir 
   user node['current_user']
   action :nothing
 end
