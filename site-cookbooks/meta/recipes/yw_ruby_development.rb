e6dbecaa3dc65e091992f88fb13ffc4d47e7bb97
Mike Karolow <mike@yesware.com>
2013-09-11 16:13:35 -0400

    EC2 tools

--- site-cookbooks/meta/recipes/yw_ruby_development.rb
+++ site-cookbooks/meta/recipes/yw_ruby_development.rb
@@ -1,8 +1,8 @@
 # DONE include_recipe "pivotal_workstation::git"
 # SKIP include_recipe "pivotal_workstation::rvm"
-# SKIP homebrew? Pivitol's needs rewrite without bash_it
+# DONE homebrew? Pivitol's needs rewrite without bash_it
 
-# DOIT git-projects? Overide workspace dir?
+# DONE git-projects? Overide workspace dir?
 # DOIT ec2-tools and ENV vars (look at the second step in theres)
 # DONE Chef repos 
 # WRITEIT Tools repo, symlinks to ~/bin, PATH to ~/bin
@@ -12,3 +12,4 @@ include_recipe "sprout-osx-base::homebrew"
 include_recipe "pivotal_workstation::git"
 include_recipe "pivotal_workstation::git_projects"
 include_recipe "yw_workstation::yw_tools"
+include_recipe "yw_workstation::ec2_api_tools"
