46bd49613da108d37cac09d88e5eff5a3744e553
Mike Karolow <mike@yesware.com>
2013-09-11 21:00:14 -0400

    Cleanup before PR

--- site-cookbooks/meta/recipes/yw_ruby_development.rb
+++ site-cookbooks/meta/recipes/yw_ruby_development.rb
@@ -1,13 +1,3 @@
-# DONE include_recipe "pivotal_workstation::git"
-# SKIP include_recipe "pivotal_workstation::rvm"
-# DONE homebrew? Pivitol's needs rewrite without bash_it
-
-# DONE git-projects? Overide workspace dir?
-# DONE ec2-tools and ENV vars (look at the second step in theres)
-# DONE Chef repos 
-# DONE Tools repo, symlinks to ~/bin, PATH to ~/bin
-# WRITEIT Heroku toolbelt
-
 include_recipe "sprout-osx-base::homebrew"
 include_recipe "pivotal_workstation::git"
 include_recipe "pivotal_workstation::git_projects"
